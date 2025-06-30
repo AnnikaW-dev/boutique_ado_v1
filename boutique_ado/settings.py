42781fe64da1dcbbc64fb42151ae43657e9e7ffd
AnnikaW-dev <warmannika@gmail.com>
2025-06-30 14:45:54 +0200

    Add checkout views and templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 
@@ -64,6 +67,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutique_ado.urls'
+
+CRISPY_TEMPLATE_PACK ='bootstrap4'
 
 TEMPLATES = [
     {
@@ -82,6 +87,10 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins':[
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
