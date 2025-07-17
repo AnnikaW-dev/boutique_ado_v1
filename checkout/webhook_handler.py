5d816817b405dbadd82756a39b87b9e9cb8d852c
AnnikaW-dev <warmannika@gmail.com>
2025-07-17 14:30:37 +0200

    Add webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 class StripeWH_Handler:
-    """Handle Stripe webhooks"""
+    """Handle Stripe webhooks """
 
     def __init__(self, request):
         self.request = request
