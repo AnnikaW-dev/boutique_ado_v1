3a4ebbd118f7f82cf7153e6c586144d19b4b9163
AnnikaW-dev <warmannika@gmail.com>
2025-07-31 18:12:42 +0200

    Add debug section in settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -225,3 +225,21 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import logging
+import sys
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
